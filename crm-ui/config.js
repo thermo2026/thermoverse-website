a4c04f546721f80799ee82f1114590e3a3104321
thermo2026 <thermo2026@users.noreply.github.com>
2026-09-19 13:26:34 +0800

    feat: integrate CRM frontend into Amplify deployment and configure inquiry API

---
+++ crm-ui/config.js
@@ -0,0 +1,6 @@
+window.THERMO_CRM_CONFIG = {
+  apiBaseUrl: 'https://ie64gq4vg7.execute-api.ap-southeast-2.amazonaws.com',
+  region: 'ap-southeast-2',
+  userPoolId: 'ap-southeast-2_Rl6THekyg',
+  clientId: '3uckk4sktqatq0nlglc0fnp911'
+};
